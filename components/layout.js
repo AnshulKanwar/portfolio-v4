9b32514ab305122392647539bfa085733a8e68ed
Anshul Kanwar <4n5hu1@protonmail.com>
2022-09-08 14:40:16 +0530

    Added meta tags

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
+
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen text-[#212B3F] bg-[#CCD0D9] dark:text-[#CCD0D9] dark:bg-[#212B3F]">
-      {children}
+    <div>
+      <Head>
+        <title>Anshul Kanwar</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#CCD0D9" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#212B3F" media="(prefers-color-scheme: dark)" />
+      </Head>
+      <div className="min-h-screen text-[#212B3F] bg-[#CCD0D9] dark:text-[#CCD0D9] dark:bg-[#212B3F]">
+        {children}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Sidebar from "../components/sidebar";
 import Hero from "../components/hero";
 import Layout from "../components/layout";
@@ -6,10 +5,6 @@ import Layout from "../components/layout";
 export default function Home() {
   return (
     <Layout>
-      <Head>
-        <title>Anshul Kanwar</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="min-h-screen grid grid-cols-[1fr_2fr] py-20">
         <div className="grid place-items-center border-r border-[#96A0B3] dark:border-[#3D4F74]">
           <Sidebar />
